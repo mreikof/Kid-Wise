c9edd306a23dcbb78b366e779e111c910d762549
davemtadesse <31293749+davemtadesse@users.noreply.github.com>
2017-10-25 19:28:27 -0500

    new update

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-// First day weather tracker
+//Declare global variables here
+var todayTemps;
+var todayForecast;
 var show;
 var lon;
 var lat;
@@ -21,6 +23,10 @@ $("#today").on("click", function(event) {
     var icon = response.list[0].weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
     $(".icon").html("<img class=icons src='" + iconUrl + "'>");
+    todayTemps = response.list[0].temp.day;
+    todayForecast = response.list[0].weather[0].main;
+
+    getDressed();
     // Added breezometer air quality api and sinked it to the response from the weather api
     var lon = response.city.coord.lon;
     var lat = response.city.coord.lat;
@@ -36,6 +42,7 @@ $("#today").on("click", function(event) {
       console.log(response.random_recommendations.children);
       $("#quality").text(response.breezometer_description);
       $("#children").text(response.random_recommendations.children + ".");
+
     });
   });
 });
@@ -116,3 +123,56 @@ $("#third").on("click", function(event) {
 
   });
 });
+function displayImage(placement, id, src) {
+  $(placement).append("<img id='" + id + "' src='images/" + src + "' height='100px' width='auto'>");
+}
+
+function getDressed() {
+
+  var girlclothes = {
+    hotWeather: ["girlDress1.png", "girlTank1.png", "boyShorts1.png", "girlSkirt1.png", "girlHat1.png", "boyHat2.png"],
+    hotWeatherCat: ["#tops", "#tops", "#bottoms", "#bottoms", "#accessories-1", "#accessories-1"],
+    coldWeather: ["girlShirt1.png", "girlShirt2.png", "boyPants1.png", "boyPants2.png", "boyHat2.png"],
+    coldWeatherCat: ["#tops", "#tops", "#bottoms", "#bottoms", "#accessories-1"],
+    colderWeather: ["girlShirt2.png", "girlShirt3.png", "boyPants1.png", "boyPants2.png", "scarf2.png"],
+    colderWeatherCat: ["#tops", "#tops", "#bottoms", "#bottoms", "#accessories-1"]
+  };
+
+  var boyclothes = {
+    hotWeather: ["boyTshirt1.png", "boyTshirt2.png", "boyShorts1.png", "boyShorts2.png", "boyHat1.png", "boyHat2.png"],
+    coldWeather: ["boyShirt1.png", "boyTshirt2.png", "boyPants1.png", "boyPants2.png"],
+    colderWeather: ["boyShirt2.png", "boyShirt3.png", "boyPants1.png", "boyPants2.png", "scarf1.png"]
+  };
+
+  var character = ["girl.png", "boy.png"];
+  var genderClothes = "girlclothes";
+
+  console.log(todayTemps);
+
+  //build conditionals that displays the correct graphics
+  $("#character").append("<img id='gender' src='images/girl2.png' height='500px' width='auto'>");
+
+  for (var i = 0; i < girlclothes.hotWeather.length; i++) {
+    if (todayTemps > 80) {
+      $("#overall-results").text("It's warm! Be sure to dress lightly. Help dress me!");
+      displayImage(girlclothes.hotWeatherCat[i], girlclothes.hotWeatherCat[i] + "-" + i, girlclothes.hotWeather[i]);
+    } else if (todayTemps > 60) {
+      $("#overall-results").text("It may be cooler! Be sure to dress warmly. Help dress me!");
+      displayImage(girlclothes.coldWeatherCat[i], girlclothes.coldWeatherCat[i] + "-" + i, girlclothes.coldWeather[i]);
+    } else if (todayTemps > 30) {
+      $("#overall-results").text("It will be COLD! Be sure to dress as warm as you can. Help dress me!");
+      displayImage(girlclothes.colderWeatherCat[i], girlclothes.colderWeatherCat[i] + "-" + i, girlclothes.colderWeather[i]);
+    } else {
+      alert("Oops");
+    }
+  }
+}
+// Function to clear out the right side panel - the clothes choices - should the user want to try a different location
+function getClear() {
+  $("#tops-0").html(" ");
+  $("#tops-1").html(" ");
+  $("#bottoms-2").html(" ");
+  $("#bottoms-3").html(" ");
+  $("#accessories-1-4").html(" ");
+  $("#accessories-1-5").html(" ");
+}
