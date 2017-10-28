d97adf0493b3ed1ab957256113a76ac8082ea667
mreikof <mike.reikofski@gmail.com>
2017-10-28 15:11:58 -0500

    img update

--- game.js
+++ game.js
@@ -252,3 +252,17 @@ function getClear() {
   $("#footwear").html(" ");
   $("#character").html(" ");
 }
+//Function to drag clothing images to image body
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
