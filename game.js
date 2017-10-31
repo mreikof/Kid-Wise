178622e6e704c4f9bd624f37c4d0647784ef57ff
mreikof <mike.reikofski@gmail.com>
2017-10-31 11:58:40 -0500

    image add

--- game.js
+++ game.js
@@ -252,18 +252,3 @@ function getClear() {
   $("#footwear").html(" ");
   $("#character").html(" ");
 }
-//Function to drag clothing images to image body
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("image", ev.target.id);
-}
-
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("image");
-    ev.target.appendChild(document.getElementById(data));
-}
